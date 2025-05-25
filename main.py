16a226946140f540a4f4a071bf7a4892aa409081
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-05-25 15:49:23 -0400

    fixed

--- main.py
+++ main.py
@@ -4,20 +4,11 @@ import time
 def toggle(var):
     return not var
 
-led = Pin(15, Pin.OUT)
 button = Pin(16, Pin.IN, Pin.PULL_UP)
-buttononoff = False
 
 while True:
     if button.value() == 0: 
         print("Is pressed")
-        buttononoff = toggle(buttononoff)
         time.sleep(0.1)
     else:
         print("Not pressed")
-    
-    if buttononoff == True:
-        led.value(1)
-    elif buttononoff == False:
-        led.value(0)
-    time.sleep(0.1)
